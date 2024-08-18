6130f4a2ff532dc423edd13f96f94aeceafd9530
Bhupendra Singh Chaudhary <bhupendra.chaudhary@ucdconnect.ie>
2024-08-18 17:03:11 +0100

    created new sign language translator code and added audios.

--- config/config.py
+++ config/config.py
@@ -4,17 +4,17 @@ import os
 # Directory paths
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Absolute path of base directory
 DATASET_PATH = os.path.join(BASE_PATH, 'dataset')  # Path to the dataset directory
-MODEL_PATH = os.path.join(BASE_PATH, 'models')  # Path to the models directory
-LOGS_PATH = os.path.join(BASE_PATH, 'logs')  # Path to the models directory
 LABELS_PATH = os.path.join(BASE_PATH, 'labels.txt')  # Path to the labels directory
+ARTIFACTS_PATH = os.path.join(BASE_PATH, 'artifacts')  # New path for storing pickle files and models
 
 # Constants for data collection
-NUM_SAMPLES = 3  # Number of samples to collect for each label
-NUM_FRAMES = 30  # Number of frames per sample
+NUM_SAMPLES = 50  # Number of samples to collect for each label
 
 # Model configuration
-MODEL_NAME = 'model.keras'
+# MODEL_NAME = 'model.keras'
+MODEL_NAME = 'model.pkl'
+DATA_PICKLE_NAME = 'data.pickle'
 
 # Logging configuration
 LOGGING_LEVEL = 'INFO'
-LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"    # Format for log messages
+LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"  # Format for log messages
