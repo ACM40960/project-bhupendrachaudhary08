c0f4db2af67f87b707bcbc1173eb60d963ecb7c7
Bhupendra Singh Chaudhary <bhupendra.chaudhary@ucdconnect.ie>
2024-08-20 06:10:17 +0100

    Code refactor and updated readme file

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -1,63 +1,75 @@
-import pickle
-
 import cv2
-import matplotlib.pyplot as plt
+import logging
 import mediapipe as mp
 import os
-from config.config import ARTIFACTS_PATH, DATA_PICKLE_NAME, DATASET_PATH, MODEL_NAME
+import pickle
 
+from config import ARTIFACTS_DIR, DATASET_DIR, DATA_PICKLE_FILENAME, LOGGING_FORMAT, LOGGING_LEVEL
+
+# Configure logging settings
+logging.basicConfig(level=LOGGING_LEVEL, format=LOGGING_FORMAT)
+
+# Initialize MediaPipe Hands solution
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 
-def create_dataset():
-    data = []
-    labels = []
-    for dir_ in os.listdir(DATASET_PATH):
-        if dir_ == ".DS_Store":  # Skip .DS_Store file
-            continue
-        dir_path = os.path.join(DATASET_PATH, dir_)
-        for img_path in os.listdir(dir_path):
-            if img_path == ".DS_Store":  # Skip .DS_Store file
+def create_dataset() -> None:
+    """
+    Create a dataset from the captured images, extracting hand landmarks using MediaPipe.
+
+    :return: None
+    """
+    try:
+        data = []
+        labels = []
+        for label in os.listdir(DATASET_DIR):
+            label_dir = os.path.join(DATASET_DIR, label)
+            # Skip .DS_Store file or if it is not a directory
+            if label == ".DS_Store" or not os.path.isdir(label_dir):
                 continue
-            data_aux = []
-            x_coords, y_coords, z_coords = [], [], []
-            image = cv2.imread(os.path.join(DATASET_PATH, dir_, img_path))
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            for img_file in os.listdir(label_dir):
+                if img_file.endswith(".jpg"):
+                    data_aux = []
+                    x_coords, y_coords, z_coords = [], [], []
+                    image_path = os.path.join(label_dir, img_file)
+                    image = cv2.imread(image_path)
+                    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    # Perform prediction with the hands model
+                    result = hands.process(image_rgb)
+                    if result.multi_hand_landmarks:
+                        for hand_landmarks in result.multi_hand_landmarks:
+                            # mp_drawing.draw_landmarks(
+                            #     image_rgb,
+                            #     hand_landmarks,
+                            #     mp_hands.HAND_CONNECTIONS,
+                            # )
+                            for lm in hand_landmarks.landmark:
+                                x_coords.append(lm.x)
+                                y_coords.append(lm.y)
+                                z_coords.append(lm.z)
 
-            # Perform prediction with the hands model
-            result = hands.process(image_rgb)
-            if result.multi_hand_landmarks:
-                for hand_landmarks in result.multi_hand_landmarks:
-                    # mp_drawing.draw_landmarks(
-                    #     image_rgb,
-                    #     hand_landmarks,
-                    #     mp_hands.HAND_CONNECTIONS,
-                    # )
-                    for lm in hand_landmarks.landmark:
-                        x_coords.append(lm.x)
-                        y_coords.append(lm.y)
-                        z_coords.append(lm.z)
-
-                    x_min, y_min, z_min = min(x_coords), min(y_coords), min(z_coords)
-                    for lm in hand_landmarks.landmark:
-                        data_aux.extend([lm.x - x_min, lm.y - y_min, lm.z - z_min])
-                data.append(data_aux)
-                labels.append(dir_)
-            # plt.figure()
-            # plt.imshow(image_rgb)
-            # break
-    os.makedirs(ARTIFACTS_PATH, exist_ok=True)  # Ensure artifacts folder exists
-    with open(os.path.join(ARTIFACTS_PATH, DATA_PICKLE_NAME), 'wb') as f:
-        pickle.dump({'data': data, 'labels': labels}, f)
-    # plt.show()
+                            x_min, y_min, z_min = min(x_coords), min(y_coords), min(z_coords)
+                            for lm in hand_landmarks.landmark:
+                                data_aux.extend([lm.x - x_min, lm.y - y_min, lm.z - z_min])
+                        data.append(data_aux)
+                        labels.append(label)
+                    # plt.figure()
+                    # plt.imshow(image_rgb)
+                    # break
+        os.makedirs(ARTIFACTS_DIR, exist_ok=True)  # Ensure artifacts folder exists
+        with open(os.path.join(ARTIFACTS_DIR, DATA_PICKLE_FILENAME), 'wb') as f:
+            pickle.dump({'data': data, 'labels': labels}, f)
+        logging.info(f"Dataset created and saved to {os.path.join(ARTIFACTS_DIR, DATA_PICKLE_FILENAME)}")
+        # plt.show()
+    except Exception as e:
+        logging.error(f"An error occurred while creating the dataset: {e}")
 
 
 def main() -> None:
     """
-    Main function to start data collection for each label.
+    Main function to start data creation process.
     """
     try:
         create_dataset()
--- src/utils.py
+++ src/utils.py
@@ -1,37 +1,29 @@
 # Import necessary packages/dependencies
-import cv2
 import logging
-import os
-from typing import Tuple, Dict, Any
+from typing import Dict
 
-from config.config import LABELS_PATH
+from config import LABELS_FILE
 
 
 def load_labels() -> Dict[int, str]:
     """
-    Load labels from the labels file.
-    :return: Dictionary mapping indices to labels.
+    Load labels from the labels file and return them as a dictionary.
+
+    :return: A dictionary mapping indices (int) to labels (str).
     """
     try:
         sign_labels: list[str] = []
         # Open the labels file and read all the lines
-        with (open(LABELS_PATH, "r") as file):
-            # sign_labels = {
-            #     idx: line.strip()
-            #     for idx, line in enumerate(file)
-            #     if not line.startswith('#') and line.strip()
-            # }
+        with (open(LABELS_FILE, "r") as file):
             for line in file.readlines():
                 line = line.strip()
-                # Skip lines that are comments
+                # Skip comments and empty lines
                 if line.startswith('#') or not line.strip():
                     continue
                 # Add the label to the sign_labels list
                 sign_labels.append(line)
-            # Create a dictionary mapping indices to labels
+            # Return a dictionary where keys are indices and values are labels
         return {idx: label for idx, label in enumerate(sign_labels)}
-        # return sign_labels
     except Exception as e:
-        # Log an error if an exception occurs
         logging.error(f"Error loading labels: {e}")
         return {}
